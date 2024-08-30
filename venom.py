1b990afef2cd06e6b42a9c6aa214c6f89796160c
DEADLINEMODDDOS <polaydimyr@gmail.com>
2024-08-30 10:09:04 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7089318460:AAEAW3a5kkhDhUX_3mssueliYsb02PHbW3w'
+ADMIN_ID = '6295067084'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
